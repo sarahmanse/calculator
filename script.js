9a1eaa7a2f747ec693fe4da4ee78b5bdc15ca79d
Sarahmanse <sarahmanse24@gmail.com>
2022-12-08 15:10:09 +0100

    update

---
+++ script.js
@@ -0,0 +1,115 @@
+const buttons = document.querySelectorAll('button');
+const display = document.querySelector('.display');
+
+let displayValue = '0';
+let firstNumber = null;
+let secondNumber = null;
+let op = null;
+let result = 'error';
+
+function processInput() {
+  for(let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', function() {
+      if (buttons[i].classList.contains('number')) {
+        inputNumber(buttons[i].innerText);
+        updateDisplay();
+      } else if (buttons[i].classList.contains('operator')) {
+        inputOperator(buttons[i].innerText);
+        updateDisplay();
+      } else if (buttons[i].classList.contains('equals')) {
+        inputEquals();
+        updateDisplay();
+      } else if (buttons[i].classList.contains('clear')) {
+        clearDisplay();
+        updateDisplay();
+      }
+    }
+  )}
+}
+
+processInput();
+
+function updateDisplay() {
+  if(displayValue.length > 8) {
+    display.innerText = displayValue.substring(0, 8);
+  } else {
+    display.innerText = displayValue;
+  }
+}
+
+function inputNumber(number) {
+  if (op === null) {
+    if (displayValue === '0' && displayValue.length == 1) {
+      displayValue = number;
+    } else {
+      displayValue += number;
+    }
+  } else {
+    if (displayValue === firstNumber) {
+      displayValue = number;
+    } else {
+      displayValue += number;
+    }
+  }  
+}
+
+function inputOperator(operator) {
+  if (op === null) {
+    firstNumber = displayValue;
+    op = operator;
+  } else {
+    secondNumber = displayValue;
+    result = operate(op, Number(firstNumber), Number(secondNumber));
+    firstNumber = roundAccurately(result, 15).toString();
+    op = operator;
+    if (result === 'error'){
+      displayValue = 'error';
+    } else {
+      displayValue = roundAccurately(result, 15).toString();
+    }
+  }  
+}
+
+function inputEquals() {
+  if (firstNumber === null) {
+    displayValue = result;
+  } else {
+    secondNumber = displayValue;
+    result = operate(op, Number(firstNumber), Number(secondNumber));
+    if (result === 'error'){
+      displayValue = 'error';
+    } else {
+      displayValue = roundAccurately(result, 15).toString();
+    }
+    firstNumber = displayValue;
+    op = null;
+  }
+}
+
+function clearDisplay() {
+  displayValue = '0';
+  firstNumber = null;
+  secondNumber = null;
+  op = null;
+  result = 'error';
+}
+
+function operate(operator, a, b) {
+  if (operator === '+'){
+    return a + b;
+  } else if (operator === '-') {
+    return a - b;
+  } else if (operator === '*') {
+    return a * b;
+  } else if (operator === '/') {
+    if (b === '0') {
+      return 'error';
+    } else {
+      return a / b;
+    }
+  }
+}
+
+function roundAccurately(number, places) {
+  return parseFloat(Math.round(number + 'e' + places) + 'e-' + places);
+}
